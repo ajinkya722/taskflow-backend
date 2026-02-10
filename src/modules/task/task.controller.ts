34faf0393b4eb8906a9bf81f08fc0886685ddfbc
Ajinkya Pawar <arpawar722@gmail.com>
2026-02-10 23:47:36 +0530

    Add validations for creatTask API

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
 import * as taskService from "./task.service";
+import { createTaskSchema } from "./task.schema";
+import { error } from "node:console";
 
 export const createTask = (req: Request, res: Response) => {
-  const task = taskService.createTask(req.body);
+  const parsedBody = createTaskSchema.safeParse(req.body);
+  if (!parsedBody.success) {
+    return res.status(400).json({
+      status: 400,
+      error: "Bad_Request",
+      message: "Validation failed",
+      details: parsedBody.error.issues.map((issue) => ({
+        field: issue.path[0],
+        message: issue.message,
+    })),
+    });
+  }
+  const task = taskService.createTask(parsedBody.data);
   res.status(201).json(task);
 };
 
